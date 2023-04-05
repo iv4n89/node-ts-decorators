9af836bce14fa0421cf343bb62804fb760f80f33
iv4n89 <ibetanzos@aiudo.es>
2023-04-05 20:14:35 +0200

    fix activity section service

--- Services/Activity/ActivityPart/ActivitySection.service.ts
+++ Services/Activity/ActivityPart/ActivitySection.service.ts
@@ -2,6 +2,7 @@ import { Repository } from "typeorm";
 import { ActivitySection } from "../../../Models/Activity/ActivityPart/ActivitySection";
 import { Server } from "../../../Server/Server";
 import { BaseService } from "../../BaseService.service";
+import { PossibleResultService } from "./PossibleResult.service";
 
 
 
@@ -18,6 +19,21 @@ export class ActivitySectionService extends BaseService<ActivitySection> {
         }
 
         return ActivitySectionService.Instance;
+    }
+
+    public async create(attributes: ActivitySection): Promise<ActivitySection> {
+        const entity = this.repository.create(attributes);
+        if (!!attributes.possibleResults) {
+            const resultRepo = PossibleResultService.instance;
+            const results = [];
+            for (const result of attributes.possibleResults) {
+                const newResult = await resultRepo.create(result);
+                results.push(newResult.id);
+            }
+            entity.possibleResults = results;
+        }
+
+        return await this.repository.save(entity);
     }
 
 }
